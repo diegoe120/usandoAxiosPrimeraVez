05cafa4c86d08bb2b799a7a2e3f5ab70a40a6385
diegoe120 <38639302+diegoe120@users.noreply.github.com>
2023-03-31 10:24:50 -0300

    transicion a axios

--- app.js
+++ app.js
@@ -18,33 +18,34 @@ btnAnterior.addEventListener('click', () => {
 
 const cargarPeliculas = async () => {
     try {
-        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
+        const respuestaPeli = await axios.get(`https://api.themoviedb.org/3/movie/550?api_key=4047f87b5c7f18194cb9027abda2dc48&Language=es-AR`)
+            .then();
 
-        console.log(respuesta);
+        console.log(respuestaPeli);
 
         // Si la respuesta es correcta
-        if (respuesta.status === 200) {
-            const datos = await respuesta.json();
+        // if (respuestaPeli.status === 200) {
+        //     const datos = await respuestaPeli;
 
-            let peliculas = '';
-            datos.results.forEach(pelicula => {
-                peliculas += `
-					<div class="pelicula">
-						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
-						<h3 class="titulo">${pelicula.title}</h3>
-					</div>
-				`;
-            });
+        //     let peliculas = '';
+        //     datos.results.forEach(pelicula => {
+        //     peliculas += `
+        // 		<div class="pelicula">
+        // 			<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+        // 			<h3 class="titulo">${pelicula.title}</h3>
+        // 		</div>
+        // 	`;
+        // });
 
-            document.getElementById('contenedor').innerHTML = peliculas;
+        // document.getElementById('contenedor').innerHTML = peliculas;
 
-        } else if (respuesta.status === 401) {
-            console.log('Pusiste la llave mal');
-        } else if (respuesta.status === 404) {
-            console.log('La pelicula que buscas no existe');
-        } else {
-            console.log('Hubo un error y no sabemos que paso');
-        }
+        // } else if (respuesta.status === 401) {
+        //     console.log('Pusiste la llave mal');
+        // } else if (respuesta.status === 404) {
+        //     console.log('La pelicula que buscas no existe');
+        // } else {
+        //     console.log('Hubo un error y no sabemos que paso');
+        // }
 
     } catch (error) {
         console.log(error);
